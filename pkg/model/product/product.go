44dba9c92d7030810cdeb05a3cb09ed24171e0e9
liam <liamyabou@outlook.com>
2020-05-05 17:30:40 +0800

    Write the unit test for the method: `BulkilyInsert()` in products.

--- pkg/model/product/product.go
+++ pkg/model/product/product.go
@@ -1,5 +1,11 @@
 package product
 
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
 func NewRow() *Row {
 	return &Row{}
 }
@@ -15,7 +21,8 @@ type Row struct {
 }
 
 type Rows struct {
-	Set []*Row
+	Set      []*Row
+	RecordId int
 }
 
 // Access the data directily instead of going throuth the pointer.
@@ -25,4 +32,20 @@ func (p *Rows) RemovePointers(set []*Row) (rawSet []Row) {
 		rawSet = append(rawSet, *post)
 	}
 	return rawSet
+}
+
+func (r *Rows) BulkilyInsert(productSet []*Row, db *sql.DB) (*Rows, error) {
+	r.Set = productSet
+	valueStrings := make([]string, 0, len(r.Set))
+	valueArgs := make([]interface{}, 0, len(r.Set)*2)
+	for i, post := range r.Set {
+		valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d)", i*2+1, i*2+2))
+		valueArgs = append(valueArgs, post.Name)
+		valueArgs = append(valueArgs, post.Rank)
+	}
+
+	// Note: `RETURNIN ID` in this statement will return the id of the first row inserted into the DB.
+	stmt := fmt.Sprintf("INSERT INTO products (name, rank) VALUES %s RETURNING id", strings.Join(valueStrings, ","))
+	err := db.QueryRow(stmt, valueArgs...).Scan(&r.RecordId)
+	return r, err
 }
--- pkg/model/product/product_test.go
+++ pkg/model/product/product_test.go
@@ -2,12 +2,48 @@ package product_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
+	"top100-scrapy/pkg/app"
 	"top100-scrapy/pkg/model/product"
 	"top100-scrapy/pkg/test"
 
+	"github.com/khaiql/dbcleaner"
+	"github.com/khaiql/dbcleaner/engine"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
+
+var (
+	Cleaner = dbcleaner.New()
+	dsn     = os.Getenv("TOP100_DB_TEST_DSN")
+)
+
+type productSuite struct {
+	suite.Suite
+}
+
+// Run before the tests in the suite are run.
+func (p *productSuite) SetupSuite() {
+	// Init and set db cleanup engine
+	psql := engine.NewPostgresEngine(dsn)
+	Cleaner.SetEngine(psql)
+}
+
+// Run before each test in the suite.
+func (p *productSuite) SetupTest() {
+	Cleaner.Acquire("products")
+}
+
+// Run after each test in the suite.
+func (p *productSuite) TearDownTest() {
+	Cleaner.Clean("products")
+}
+
+// Run after all the tests in the suite have been run.
+func (p *productSuite) TearDownSuite() {
+	Cleaner.Close()
+}
 
 func TestRemovePointers(t *testing.T) {
 	expected := []product.Row{
@@ -22,4 +58,24 @@ func TestRemovePointers(t *testing.T) {
 	actual := products.RemovePointers(products.Set)
 	failedMsg := fmt.Sprintf("Failed, expected the raw set: %v, got the set: %v", expected, actual)
 	assert.Equal(t, expected, actual, failedMsg)
+}
+
+func (p *productSuite) TestBulkilyInsert() {
+	defer app.Finalize()
+	scrapedProducts := product.NewRows()
+	scrapedProducts.Set = test.CannedProductsSet
+	products := product.NewRows()
+	products, err := products.BulkilyInsert(scrapedProducts.Set, app.DBconn)
+	if err != nil {
+		p.T().Errorf("Failed to inster the products, error: %v", err)
+	} else {
+		expected := scrapedProducts.RemovePointers(scrapedProducts.Set)
+		actual := products.RemovePointers(products.Set)
+		failedMsg := fmt.Sprintf("Failed, expected the data inserted into the products: %v, got the data: %v", expected, actual)
+		assert.Equal(p.T(), expected, actual, failedMsg)
+	}
+}
+
+func TestRunSuite(t *testing.T) {
+	suite.Run(t, new(productSuite))
 }
