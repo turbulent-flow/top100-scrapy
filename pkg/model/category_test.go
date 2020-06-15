becdf621c8dad0acf88c8c301f74a4a28052dad9
liam <liamyabou@outlook.com>
2020-06-15 15:23:00 +0800

    Release the version v2.1.1-change the position of the directory `test`.

--- pkg/model/category_test.go
+++ pkg/model/category_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"github.com/LiamYabou/top100-scrapy/v2/pkg/model"
 	"github.com/LiamYabou/top100-scrapy/v2/preference"
-	"github.com/LiamYabou/top100-scrapy/v2/pkg/test"
+	"github.com/LiamYabou/top100-scrapy/v2/test"
 
 	"github.com/stretchr/testify/assert"
 )
