b710168ce5ade2b29b682bd87b58c1eaf0d58bec
liam <liamyabou@outlook.com>
2020-06-15 15:18:19 +0800

    Release the version v2.1.0-change the position of the directory `preference`.

--- pkg/model/category_test.go
+++ pkg/model/category_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 	"github.com/LiamYabou/top100-scrapy/v2/pkg/model"
-	"github.com/LiamYabou/top100-scrapy/v2/pkg/preference"
+	"github.com/LiamYabou/top100-scrapy/v2/preference"
 	"github.com/LiamYabou/top100-scrapy/v2/pkg/test"
 
 	"github.com/stretchr/testify/assert"
