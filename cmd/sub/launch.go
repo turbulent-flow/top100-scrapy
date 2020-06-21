754df84e40c91f36c345d462735787813501c302
liam <liamyabou@outlook.com>
2020-06-21 18:58:45 +0800

    adjustment

--- cmd/sub/launch.go
+++ cmd/sub/launch.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"strconv"
 	"github.com/LiamYabou/top100-scrapy/v2/app"
 	"github.com/LiamYabou/top100-scrapy/v2/preference"
+	"github.com/LiamYabou/top100-scrapy/v2/variable"
 	"github.com/LiamYabou/top100-scrapy/v2/pkg/rabbitmq"
 )
 
 func main() {
 	defer app.Finalize()
+	c, _ := strconv.Atoi(variable.Concurrency)
 	opts := &preference.Options{
 		DB:            app.DBpool,
 		AMQP:          app.AMQPconn,
-		Concurrency:   25,
-		PrefetchCount: 100,
+		Concurrency:   c,
+		PrefetchCount: (c * 2),
 	}
 	opts = preference.LoadOptions(preference.WithOptions(*opts))
 	rabbitmq.RunSubscriber(opts)
