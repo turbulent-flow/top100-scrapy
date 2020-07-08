1726daa8995c534b2ee338838dca72884fd405ab
liam <liamyabou@outlook.com>
2020-07-08 11:55:43 +0800

    Revise the lines.

--- app/final.go
+++ app/final.go
@@ -12,7 +12,7 @@ func Finalize() {
   if variable.Env == "development" {
 		logFile.Close()
 	} else {
-		sentry.Recover() // Capture the unhandled panic
+    sentry.Recover() // Capture the unhandled panic
     sentry.Flush(5 * time.Second) // Set the timeout to the maximum duration the program can afford to wait.
 	}
 }
