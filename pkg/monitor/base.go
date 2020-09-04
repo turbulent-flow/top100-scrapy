dc6d356ddfb34ede7badd7c6c9b10a55d552cb8e
liam <liamyabou@outlook.com>
2020-09-05 00:28:32 +0800

    Troubleshoot the issues of NewRelic.

--- pkg/monitor/base.go
+++ pkg/monitor/base.go
@@ -4,15 +4,26 @@ import (
 	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/LiamYabou/top100-scrapy/variable"
 	"github.com/getsentry/sentry-go"
+	"os"
 )
 
 func InitNewRelic() (newRelicApp *newrelic.Application, err error) {
 	if variable.Env == "development" {
 		return
 	}
+	// newRelicLogPath := fmt.Sprintf("%s/logs/go-agent.log", variable.AppURI)
+	// w, err := os.OpenFile(newRelicLogPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	// factors := logger.Factors{
+	// 	"newRelicLogPath":  newRelicLogPath,
+	// }
+	// logger.Info("troubleshoot the newRelic", factors)
+	// if nil == err {
+	// 	newrelic.NewDebugLogger(w)
+	// }
 	newRelicApp, err = newrelic.NewApplication(
 		newrelic.ConfigAppName(variable.AppName),
 		newrelic.ConfigLicense(variable.NewRelicLicenseKey),
+		newrelic.ConfigDebugLogger(os.Stdout),
 	)
 	return
 }
